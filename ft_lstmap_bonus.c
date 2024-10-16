b744dd90a303b9497e9b3a7579b013842dc2d4d2
Noa Charbogne <noa.charbogne@orange.fr>
2024-10-16 13:15:34 +0200

    midi

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ncharbog <ncharbog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 17:52:47 by ncharbog          #+#    #+#             */
-/*   Updated: 2024/10/16 12:41:39 by ncharbog         ###   ########.fr       */
+/*   Updated: 2024/10/16 13:15:29 by ncharbog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*destlist;
 	t_list	*new;
+	void	*temp;
 
-	new = NULL;
+	destlist = NULL;
 	if (!lst || !f || !del)
 		return (0);
 	while (lst)
 	{
-		new = ft_lstnew(f(lst->content));
+		temp = f(lst->content);
+		if (temp)
+			new = ft_lstnew(temp);
 		if (new == NULL)
 		{
+			del(new);
 			ft_lstclear(&new, del);
 			return (0);
 		}
